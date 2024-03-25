60dc3b1c129f9af1c542ca547c640b458232df88
George <george.lipceanu@gmail.com>
2024-03-25 22:41:28 +0000

    test commit

--- src/main/java/com/example/parisroutefinder/HelloApplication.java
+++ src/main/java/com/example/parisroutefinder/HelloApplication.java
@@ -8,6 +8,11 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public static Scene changeScene(String file) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(file));
+        return new Scene(fxmlLoader.load(), 1000, 700);
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
