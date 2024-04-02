07409d054a06a5746d18032c75c093f784760062
George <george.lipceanu@gmail.com>
2024-04-02 14:26:20 +0100

    Added code for Landmark, ParisMap, Street

--- src/main/java/com/example/parisroutefinder/HelloController.java
+++ src/main/java/com/example/parisroutefinder/HelloController.java
@@ -13,6 +13,15 @@ public class HelloController implements Initializable {
     protected void mainButton() {
         HelloApplication.mainStage.setScene(HelloApplication.mainS);
         MainController.mainController.parisMap = MainController.mainController.mapImageView.getImage();
+
+        //Adding Landmarks to Hashmap based on where they are on the map, using index on map as
+        MainController.mainController.landmarks.put(118, new Landmark("Notre Dam", 364,238,10));
+        MainController.mainController.landmarks.put(118, new Landmark("Arc de Triomphe", 122,102,10));
+        MainController.mainController.landmarks.put(118, new Landmark("Sacre-Coeur", 324,8,10));
+        MainController.mainController.landmarks.put(118, new Landmark("Louvre", 303,189,10));
+        MainController.mainController.landmarks.put(118, new Landmark("Opera Garnier", 292,134,10));
+        MainController.mainController.landmarks.put(118, new Landmark("Catacombs", 282,361,10));
+
     }
 
     @Override
--- src/main/java/com/example/parisroutefinder/Street.java
+++ src/main/java/com/example/parisroutefinder/Street.java
@@ -13,4 +13,36 @@ public class Street {
         this.startLandmark = start;
         this.endLandmark = end;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public Landmark getStartLandmark() {
+        return startLandmark;
+    }
+
+    public void setStartLandmark(Landmark startLandmark) {
+        this.startLandmark = startLandmark;
+    }
+
+    public Landmark getEndLandmark() {
+        return endLandmark;
+    }
+
+    public void setEndLandmark(Landmark endLandmark) {
+        this.endLandmark = endLandmark;
+    }
 }
