19f14df34bde9ef291ec604afaa4b3160855eacc
George <george.lipceanu@gmail.com>
2024-05-07 13:41:26 +0100

    Added benchmarking class + everything required for benchmarking

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module com.example.parisroutefinder {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jmh.core;
+    requires jmh.generator.annprocess;
 
-
-    opens com.example.parisroutefinder to javafx.fxml;
+    opens com.example.parisroutefinder to javafx.fxml,jmh.core,jmh.generator.annprocess;
     exports com.example.parisroutefinder;
+    exports com.example.parisroutefinder.jmh_generated to jmh.core;
 }
